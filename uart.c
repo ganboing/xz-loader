913c4a57d3181ce68d147e03b4a8bb99db238333
Bo Gan <ganboing@gmail.com>
2025-11-03 12:30:52 -0800

    uart8250 printer

---
+++ uart.c
@@ -0,0 +1,34 @@
+#include <stdint.h>
+#include "common.h"
+
+#define UART_BASE 0x50900000
+
+typedef struct {
+	volatile uint32_t thr;
+	uint32_t ignored[4];
+	volatile uint32_t lsr;
+} uart8250;
+
+static uart8250 * const uart = (uart8250*)UART_BASE;
+
+#define UART_LSR_THRE 0x20
+
+int uart_putchar(int c)
+{
+	while ((uart->lsr & UART_LSR_THRE) == 0)
+		;
+	uart->thr = c;
+	return c;
+}
+
+int uart_puts(const char *str)
+{
+	int len;
+
+	for (len = 0; *str; ++len)
+		putchar(*str++);
+
+	putchar('\n');
+	return len + 1;
+}
+
